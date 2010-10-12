6329fb52c17fa9ddb9198391a7b244a9c2bba043
Nick Black <dank@qemfd.net>
2010-10-11 23:28:34 -0400

    make a list of results. clean it up, etc. use a pure c interface, variety being the spice of life

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,57 @@
 #include <ios>
 #include <string>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <milleoculi.h>
 
+typedef struct result { // suitable for a linked list
+	char *arec;
+	uintmax_t looktime;
+	struct result *next;
+} result;
+
+static result *
+create_result(const std::string &arec,const uintmax_t rtime){
+	result *r;
+
+	if( (r = (result *)malloc(sizeof(*r))) ){
+		if((r->arec = strdup(arec.c_str())) == NULL){
+			free(r);
+			return NULL;
+		}
+		r->looktime = rtime;
+	}
+	return r;
+}
+
+static void
+destroy_list(result **r){
+	result *tmp;
+
+	while( (tmp = *r) ){
+		*r = tmp->next;
+		free(tmp->arec);
+		free(tmp);
+	}
+}
+
+static void
+sort_into_list(result **list,result *r){
+	r->next = *list;
+	*list = r;
+}
+
 int main(void){
+	result *list = NULL;
 	std::string req;
 	Modnsctx ctx;
 
 	std::cout << "Performing lookups (one per line, ^D to end)..." << std::endl;
 	while(getline(std::cin,req)){
 		struct timeval tv;
+		uintmax_t rtime;
+		result *r;
 
 		try{
 			std::cout << "Lookup: [" << req << "]...";
@@ -19,7 +60,15 @@ int main(void){
 			std::cerr << "RESOLUTION FAILURE" << std::endl;
 			continue;
 		}
-		std::cout << (intmax_t)tv.tv_sec * 1000000 + tv.tv_usec << std::endl;
+		rtime = (intmax_t)tv.tv_sec * 1000000 + tv.tv_usec;
+		if((r = create_result(req,rtime)) == NULL){
+			destroy_list(&list);
+			return EXIT_FAILURE;
+		}
+		std::cout << rtime << std::endl;
+		sort_into_list(&list,r);
 	}
+	// FIXME print sorted list
+	destroy_list(&list);
 	return EXIT_SUCCESS;
 }
